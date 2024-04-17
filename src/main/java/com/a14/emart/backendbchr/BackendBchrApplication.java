8aa8c1635b633d07af1bf781c7d90dd082a2efdb
Rayhan Adhisworo Jardine H <rayhan.jardine@gmail.com>
2024-04-17 11:53:25 +0700

    Initial commit.

---
+++ src/main/java/com/a14/emart/backendbchr/BackendBchrApplication.java
@@ -0,0 +1,13 @@
+package com.a14.emart.backendbchr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBchrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBchrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a14/emart/backendbchr/BackendBchrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a14.emart.backendbchr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBchrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
