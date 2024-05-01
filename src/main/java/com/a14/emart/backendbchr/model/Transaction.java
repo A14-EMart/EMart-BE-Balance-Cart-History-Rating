230df0cbc481469beea632e7acc847fa3fbb0fa4
yodh4 <cyrilus2004@gmail.com>
2024-05-01 20:16:37 +0700

    Edit attribute

--- src/main/java/com/a14/emart/backendbchr/model/Transaction.java
+++ src/main/java/com/a14/emart/backendbchr/model/Transaction.java
@@ -1,5 +1,11 @@
 package com.a14.emart.backendbchr.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
 /*
 Supermarket memiliki atribut rating.
 
@@ -15,7 +21,10 @@ daftar produk yang dibeli, total harga, rating dan komentar.
 
 Pembeli dapat memberikan rating dan juga komentar kepada Supermarket di setiap transaksi.
  */
+@Getter
+@Setter
 public class Transaction {
+    UUID id;
     Supermarket supermarket;
     Pembeli pembeli;
     String tanggalDanWaktuPembelian;
@@ -23,4 +32,7 @@ public class Transaction {
     float totalHarga;
     int rating;
     String komentar;
+
+    public Transaction() {};
+
 }
