9d5c437ba5630564b53d7dfca97ebb08b368a399
Rayhan Adhisworo Jardine H <rayhan.jardine@gmail.com>
2024-05-01 14:32:05 +0700

    [GREEN] Implemented Balance model.

--- src/main/java/com/a14/emart/backendbchr/models/Balance.java
+++ src/main/java/com/a14/emart/backendbchr/models/Balance.java
@@ -1,2 +1,27 @@
-package com.a14.emart.backendbchr.models;public class Balance {
+package com.a14.emart.backendbchr.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name="BALANCE")
+public class Balance {
+    @Id @Column(name = "USERID", nullable = false)
+    private Long userId;
+
+    @Column(name = "NOMINAL", nullable = false)
+    private BigDecimal nominal;
+
+    public Balance() {
+        // Default
+    }
+
+    public Balance(Long userId, BigDecimal nominal) {
+        this.userId = userId;
+        this.nominal = nominal;
+    }
 }
