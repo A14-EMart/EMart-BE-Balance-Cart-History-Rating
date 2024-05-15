668d2038abc5447fc82fa0095f06c5bffbe39a15
Rayhan Adhisworo Jardine H <rayhan.jardine@gmail.com>
2024-05-15 15:26:00 +0700

    [REFACTOR] Modified Balance model test to reflect changes on model.

--- src/test/java/com/a14/emart/backendbchr/models/BalanceTest.java
+++ src/test/java/com/a14/emart/backendbchr/models/BalanceTest.java
@@ -2,24 +2,24 @@ package com.a14.emart.backendbchr.models;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public class BalanceTest {
     private Balance balance;
+    UUID userId;
 
     @BeforeEach
     public void setUp() {
-        this.balance = new Balance();
-        this.balance.setUserId(42L);
-        this.balance.setNominal(BigDecimal.valueOf(20000));
+        this.userId = UUID.randomUUID();
+        BigDecimal nominal = BigDecimal.valueOf(20000);
+        this.balance = new Balance(userId, nominal);
     }
 
     @Test
     void testGetUserId() {
-        assertEquals(42, this.balance.getUserId());
+        assertEquals(this.userId, this.balance.getUserId());
     }
 
     @Test
