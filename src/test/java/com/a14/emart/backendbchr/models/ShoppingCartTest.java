66eb48611110e459c750708f1a59b6e680678025
claraa26 <clarasistaa26@gmail.com>
2024-05-23 01:16:41 +0700

    [RED] change model and create for new model test

---
+++ src/test/java/com/a14/emart/backendbchr/models/ShoppingCartTest.java
@@ -0,0 +1,85 @@
+package com.a14.emart.backendbchr.models;
+
+import com.a14.emart.backendbchr.models.ShoppingCart;
+import com.a14.emart.backendbchr.models.ShoppingCartBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ShoppingCartTest {
+    ShoppingCart shoppingCart;
+    private CartItem item1;
+    private CartItem item2;
+
+    @BeforeEach
+    void setUp() {
+        // Initialize the shopping cart
+        shoppingCart = ShoppingCart.getBuilder()
+                .setPembeliId("buyer123")
+                .setSupermarketId("supermarket456")
+                .build();
+
+        // Initialize cart items
+        item1 = CartItem.getBuilder()
+                .setPembeliId("buyer123")
+                .setProductId("product789")
+                .setAmount(2)
+                .build();
+
+        item2 = CartItem.getBuilder()
+                .setPembeliId("buyer123")
+                .setProductId("product101")
+                .setAmount(1)
+                .build();
+    }
+
+    @Test
+    public void testShoppingCart() {
+        String pembeliId = UUID.randomUUID().toString();
+        String supermarketId = UUID.randomUUID().toString();
+
+        ShoppingCart shoppingCart = new ShoppingCartBuilder()
+                .setPembeliId(pembeliId)
+                .setSupermarketId(supermarketId)
+                .build();
+
+        assertEquals(pembeliId, shoppingCart.getPembeliId());
+        assertEquals(supermarketId, shoppingCart.getSupermaketId());
+    }
+
+    @Test
+    void testAddItem() {
+        shoppingCart.addItem(item1);
+
+        assertEquals(1, shoppingCart.getItems().size());
+        assertTrue(shoppingCart.getItems().contains(item1));
+        assertEquals(shoppingCart, item1.getShoppingCart());
+
+        shoppingCart.addItem(item2);
+
+        assertEquals(2, shoppingCart.getItems().size());
+        assertTrue(shoppingCart.getItems().contains(item2));
+        assertEquals(shoppingCart, item2.getShoppingCart());
+    }
+
+    @Test
+    void testRemoveItem() {
+        shoppingCart.addItem(item1);
+        shoppingCart.addItem(item2);
+
+        shoppingCart.removeItem(item1);
+
+        assertEquals(1, shoppingCart.getItems().size());
+        assertFalse(shoppingCart.getItems().contains(item1));
+        assertNull(item1.getShoppingCart());
+
+        shoppingCart.removeItem(item2);
+
+        assertEquals(0, shoppingCart.getItems().size());
+        assertFalse(shoppingCart.getItems().contains(item2));
+        assertNull(item2.getShoppingCart());
+    }
+
+}
