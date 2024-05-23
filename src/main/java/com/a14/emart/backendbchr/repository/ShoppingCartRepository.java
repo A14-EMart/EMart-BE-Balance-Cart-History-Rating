1abaa533bd5d8283df133ee63abca9287d6d6775
claraa26 <clarasistaa26@gmail.com>
2024-05-23 14:02:01 +0700

    [GREEN] Implement shopping cart repository

--- src/main/java/com/a14/emart/backendbchr/repository/ShoppingCartRepository.java
+++ src/main/java/com/a14/emart/backendbchr/repository/ShoppingCartRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, String> {
-    Optional<ShoppingCart> findById(String id);
+    Optional<ShoppingCart> findShoppingCartByPembeliId(String pembeliId);
 }
