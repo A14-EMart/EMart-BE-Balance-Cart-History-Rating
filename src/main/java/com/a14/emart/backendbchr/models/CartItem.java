8d04381d6886f88f63fadbe79e710731b90deb9c
claraa26 <clarasistaa26@gmail.com>
2024-05-23 14:01:12 +0700

    [GREEN] implement cart item

---
+++ src/main/java/com/a14/emart/backendbchr/models/CartItem.java
@@ -0,0 +1,40 @@
+package com.a14.emart.backendbchr.models;
+import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Table(name = "cartItem")
+@Entity
+public class CartItem {
+    @Id
+    @Column(name = "pembeliId")
+    private String pembeliId;
+
+    @Column(name = "productId")
+    private String productId;
+
+    @Column(name = "amount")
+    private int amount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pembeliId", nullable = false)
+    private ShoppingCart shoppingCart;
+
+    public static CartItemBuilder getBuilder(){
+        return new CartItemBuilder();
+    }
+
+//    public CartItem(Product product, int quantity) {
+//        this.product = product;
+//        this.quantity = quantity;
+//    }
+//
+//    public void setQuantity(int quantity) {
+//        this.quantity = quantity;
+//    }
+}
