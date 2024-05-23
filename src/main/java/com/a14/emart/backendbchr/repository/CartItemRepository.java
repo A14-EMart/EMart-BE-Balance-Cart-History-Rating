cce2687a03595b4964d30d9dcc1e828bec29e37d
claraa26 <clarasistaa26@gmail.com>
2024-05-23 14:02:51 +0700

    [GREEN] Implememnt cart item repository

---
+++ src/main/java/com/a14/emart/backendbchr/repository/CartItemRepository.java
@@ -0,0 +1,13 @@
+package com.a14.emart.backendbchr.repository;
+import com.a14.emart.backendbchr.models.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, String> {
+    List<CartItem> findCartItemByPembeliId(String pembeliId);
+}
